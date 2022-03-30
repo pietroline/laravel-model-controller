88a708426e3e70b003eb4120a1dc3980897c6389
pietroline <pietrobarberi@hotmail.it>
2022-03-30 21:41:10 +0200

    prove query varie

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,7 +14,28 @@ class PageController extends Controller
 
     public function movie() {
 
+        
         $movies = Movie::all();
+
+        /*
+        // richiesta di solo i film american
+        $movies = Movie::Where("nationality", "american")->get();
+        */
+
+        /*
+        // richiesta di film con vote >= 8
+        $movies = Movie::Where("vote", ">=", "8")->get();
+        */
+
+        /*
+        // richiesta di film in ordine alfabetico
+        $movies = Movie::orderBy("title", "asc")->get();
+        */
+        
+        /*
+        // richiesta di soli 5 film americam 
+        $movies = Movie::Where("nationality", "american")->limit(5)->get();
+        */
 
         return view('movies', compact("movies"));
     }
